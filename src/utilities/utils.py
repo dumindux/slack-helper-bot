889c9478fb6a3fe49baad06ff50032c25f531526
Dumindu Buddhika <dumindukarunathilaka@gmail.com>
2018-07-03 08:04:43 +0530

    add docstrings to utils.py

--- src/utilities/utils.py
+++ src/utilities/utils.py
@@ -1,7 +1,18 @@
 """Utils"""
 import requests
 
-def rest_request(task, method, url, params=None, data=None, headers=None, auth=None):
-    print(method,url,params,auth)
-    return requests.request(method = method, url = url, params = params, 
-    json = data, headers = headers, auth = auth)
+"""
+Args:
+    method: HTTP method
+    url: HTTP url
+    params: Query parameters
+    json: JSON body
+    headers: HTTP headers
+    auth: Authorization information
+
+Returns:
+     Message to be sent to the user
+"""
+def rest_request(method, url, params=None, data=None, headers=None, auth=None):
+    return requests.request(method=method, url=url, params=params,
+                            json=data, headers=headers, auth=auth)
